68474e098b70f2165497f2bca78ba8b63c9158a5
Johan Nordström <johan@digitalvillage.se>
2022-03-30 09:49:19 +0200

    Our first Vue demo app. Much clicks.

--- 02-vue-basics/app.js
+++ 02-vue-basics/app.js
@@ -2,3 +2,17 @@
  * Vue.js Basics
  *
  */
+
+// Create our Vue app
+const app = Vue.createApp({
+	data() {
+		return {
+			count: 0,
+			msg: "Hello, Vue!",
+			salary: 10,
+		}
+	}
+})
+
+// Mount our Vue app on the element with id `app` in the DOM
+app.mount('#app')
