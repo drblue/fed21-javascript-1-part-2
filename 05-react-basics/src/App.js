f67dcc9f1485465c2db80dfbf28e125e7e20e752
Johan Nordström <johan@digitalvillage.se>
2022-04-05 14:00:46 +0200

    Our first stateful variables. Also - render a list 🤘🏻.

--- 05-react-basics/src/App.js
+++ 05-react-basics/src/App.js
@@ -1,14 +1,17 @@
+import { useState } from 'react'
 import './App.css';
 
 const App = () => {
-	let msg = "Hi mom"
-	console.log(msg)
+	const [msg, setMsg] = useState("Hi mom, I'm stateful")
+	const [clicks, setClicks] = useState(0)
+	const [posts, setPosts] = useState([
+		{ id: 1, title: "React Rocks 🤘🏻!", likes: 1337 },
+		{ id: 2, title: "JSX Rocks Even Moar 🤘🏻!", likes: 42 },
+		{ id: 3, title: "Got State?", likes: 3 },
+	])
 
 	const handleButtonClick = () => {
-		console.log("You clicked the button, good boi!")
-		console.log("Message is:", msg)
-		msg = "Bye mom"
-		console.log("Message after change is:", msg)
+		setClicks(clicks + 1)
 	}
 
 	return (
@@ -17,11 +20,29 @@ const App = () => {
 
 			<h2>{msg}</h2>
 
-			<p>You have clicked the button 0 times.</p>
+			<p>You have clicked the button {clicks} times.</p>
 
 			<button onClick={handleButtonClick} className="btn btn-success btn-lg">👆🏻 me!</button>
+
+			<button onClick={ () => { setMsg('Hi dad!') } } className="btn btn-warning btn-lg">Hi dad!</button>
+
+			<hr />
+
+			<h2>Posts</h2>
+
+			<ul>
+				{
+					posts.map( post =>
+						(
+							<li key={post.id}>
+								{post.title} ({post.likes})
+							</li>
+						)
+					)
+				}
+			</ul>
 		</div>
-	);
+	)
 }
 
 export default App;
