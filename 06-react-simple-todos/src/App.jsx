96a31f932f284d26924fbe562131bdd83b934943
Johan Nordström <johan@digitalvillage.se>
2022-04-06 15:20:12 +0200

    Simple React Todos.

---
+++ 06-react-simple-todos/src/App.jsx
@@ -0,0 +1,108 @@
+import { useState } from 'react'
+import './App.css';
+
+const App = () => {
+	const [todos, setTodos] = useState([
+		{ title: "Make coffee", completed: true },
+		{ title: "Drink coffee", completed: false },
+		{ title: "Drink MOAR coffee", completed: false },
+		{ title: "Drink ALL THE coffee", completed: false },
+	])
+
+	// input state
+	const [newTodoTitle, setNewTodoTitle] = useState('')
+
+	const toggleTodo = (todo) => {
+		todo.completed = !todo.completed
+		setTodos([...todos])
+	}
+
+	const deleteTodo = (clickedTodo) => {
+		setTodos(todos.filter(todo => todo !== clickedTodo))
+	}
+
+	const handleFormSubmit = e => {
+		// stop form from submitting
+		e.preventDefault()
+
+		// push a new todo to the todos state
+		const newTodo = { title: newTodoTitle, completed: false }
+		setTodos([...todos, newTodo])
+
+		// clear newTodoTitle state
+		setNewTodoTitle('')
+	}
+
+	return (
+		<div className="App container">
+			<h1>React Simple Todos</h1>
+
+			<div className="mb-3">
+				<form onSubmit={handleFormSubmit}>
+					<div className="input-group">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Todo title"
+							onChange={e => setNewTodoTitle(e.target.value)}
+							value={newTodoTitle}
+						/>
+						<button
+							type="submit"
+							className="btn btn-primary"
+						>
+							Create
+						</button>
+					</div>
+				</form>
+			</div>
+
+			<ul className="todolist">
+				{
+					todos.filter(todo => !todo.completed).map((todo, index) =>
+						(
+							<li className={todo.completed ? 'done' : ''} key={index}>
+								<span
+									className="todo-title"
+									onClick={() => toggleTodo(todo)}
+								>
+									{todo.title}
+								</span>
+
+								<span
+									className="todo-delete"
+									onClick={() => deleteTodo(todo)}
+								>🗑</span>
+							</li>
+						)
+					)
+				}
+			</ul>
+
+			<h2>Completed todos</h2>
+			<ul className="todolist">
+				{
+					todos.filter(todo => todo.completed).map((todo, index) =>
+						(
+							<li className={todo.completed ? 'done' : ''} key={index}>
+								<span
+									className="todo-title"
+									onClick={() => toggleTodo(todo)}
+								>
+									{todo.title}
+								</span>
+
+								<span
+									className="todo-delete"
+									onClick={() => deleteTodo(todo)}
+								>🗑</span>
+							</li>
+						)
+					)
+				}
+			</ul>
+		</div>
+	)
+}
+
+export default App;
