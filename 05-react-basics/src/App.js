8e530fc7aa642788f9b4adc88c50adbeff6af70b
Johan Nordström <johan@digitalvillage.se>
2022-04-06 11:03:33 +0200

    Show/hide salary section.

--- 05-react-basics/src/App.js
+++ 05-react-basics/src/App.js
@@ -10,6 +10,7 @@ const App = () => {
 		{ title: "Got State?", likes: 3 },
 	])
 	const [salary, setSalary] = useState(10)
+	const [showSalary, setShowSalary] = useState(true)
 
 	const handleButtonClick = () => {
 		setClicks(prevClicks => prevClicks + 1)
@@ -47,37 +48,43 @@ const App = () => {
 
 			<hr />
 
-			<p>Salary per hour: {salary} &euro;</p>
+			<button className="btn btn-primary" onClick={() => setShowSalary(!showSalary)}>Show/hide Salary</button>
 
-			{salary < 10 && (
-				<div className="alert alert-warning">You might want to get a second job?</div>
+			{showSalary && (
+				<div>
+					<p>Salary per hour: {salary} &euro;</p>
+
+					{salary < 10 && (
+						<div className="alert alert-warning">You might want to get a second job?</div>
+					)}
+
+					<div className="buttons">
+						<div className="mb-1">
+							<button
+								className="btn btn-primary btn-lg"
+								onClick={ () => changeSalary(1) }
+							>Raise 1 &euro; 🤑</button>
+
+							<button
+								className="btn btn-warning btn-lg"
+								onClick={ () => changeSalary(-1) }
+							>Decrease 1 &euro; 😢</button>
+						</div>
+
+						<div className="mb-1">
+							<button
+								className="btn btn-success btn-lg"
+								onClick={ () => changeSalary(5) }
+							>Raise 5 &euro; 🤑🤑🤑</button>
+
+							<button
+								className="btn btn-danger btn-lg"
+								onClick={ () => changeSalary(-5) }
+							>Decrease 5 &euro; 😢😢😢</button>
+						</div>
+					</div>
+				</div>
 			)}
-
-			<div className="buttons">
-				<div className="mb-1">
-					<button
-						className="btn btn-primary btn-lg"
-						onClick={ () => changeSalary(1) }
-					>Raise 1 &euro; 🤑</button>
-
-					<button
-						className="btn btn-warning btn-lg"
-						onClick={ () => changeSalary(-1) }
-					>Decrease 1 &euro; 😢</button>
-				</div>
-
-				<div className="mb-1">
-					<button
-						className="btn btn-success btn-lg"
-						onClick={ () => changeSalary(5) }
-					>Raise 5 &euro; 🤑🤑🤑</button>
-
-					<button
-						className="btn btn-danger btn-lg"
-						onClick={ () => changeSalary(-5) }
-					>Decrease 5 &euro; 😢😢😢</button>
-				</div>
-			</div>
 
 			<hr />
 
