c5905eb19b7bd1471852084cc8afd3a432d0400a
Johan Nordström <johan@digitalvillage.se>
2022-04-08 12:00:00 +0200

    Working clock.

--- 07-clock/src/App.jsx
+++ 07-clock/src/App.jsx
@@ -2,11 +2,27 @@ import { useEffect, useState } from 'react'
 import './App.css';
 
 const App = () => {
+	const [time, setTime] = useState(() => {
+		return new Date().toLocaleTimeString()
+	})
+
+	// This will only be executed when the component is mounted,
+	// and only AFTER the component has been rendered
+	useEffect(() => {
+		console.log("Starting timer...")
+
+		setInterval(() => {
+			// update time
+			const now = new Date().toLocaleTimeString()
+			console.log("tick", now)
+			setTime(now)
+		}, 1000)
+	}, [])
 
 	return (
 		<div className="container">
 			<div className="display-1 text-center">
-				00:13:37
+				{time}
 			</div>
 		</div>
 	)
