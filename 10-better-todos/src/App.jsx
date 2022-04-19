e42d58637c85b60c073ae273a0095d6d5c402976
Johan Nordström <johan@digitalvillage.se>
2022-04-19 10:41:21 +0200

    Added NavBar (menu).

--- 10-better-todos/src/App.jsx
+++ 10-better-todos/src/App.jsx
@@ -1,5 +1,6 @@
 import Container from 'react-bootstrap/Container'
-import { Routes, Route } from 'react-router-dom'
+import { Link, Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
 import TodosPage from './pages/TodosPage'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
@@ -7,14 +8,18 @@ import './App.css'
 const App = () => {
 
 	return (
-		<Container id="App">
+		<div id="App">
+			<Navigation />
 
-			<Routes>
-				<Route path="/" element={<p>Welcome home</p>} />
-				<Route path="/todos" element={<TodosPage />} />
-			</Routes>
-
-		</Container>
+			<Container>
+				<Routes>
+					<Route path="/" element={(
+						<p>Welcome home. Go to <Link to="/todos">todos</Link>.</p>
+					)} />
+					<Route path="/todos" element={<TodosPage />} />
+				</Routes>
+			</Container>
+		</div>
 	)
 }
 
