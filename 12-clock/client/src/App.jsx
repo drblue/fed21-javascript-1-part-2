3ee25d4305ad8f1329c4016b1380bbe5ee850d0a
Johan Nordström <johan@digitalvillage.se>
2022-05-13 10:01:11 +0200

    Connect to socket.io-server and say hello.

--- 12-clock/client/src/App.jsx
+++ 12-clock/client/src/App.jsx
@@ -1,7 +1,12 @@
+import socketio from 'socket.io-client'
 import Timer from './components/Timer';
 import './App.css';
 
 const App = () => {
+	const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL)
+
+	socket.emit('hewwo')
+
 	return (
 		<div className="container text-center">
 			<Timer />
